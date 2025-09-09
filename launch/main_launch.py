fce879b87cb2dd6e49c927a60ab7b250acff3df1
NaCl-Salt-12 <cla21052@byui.edu>
2025-09-09 13:52:42 -0400

    kd

--- launch/main_launch.py
+++ launch/main_launch.py
@@ -31,7 +31,7 @@ def generate_launch_description():
         name='main_ctrl_node',
         parameters= [{
             'temp_limit_c':80.0, # temperature safety limit
-            'wheels_linked':True # are the wheels controlled independantly or together
+            'wheels_linked':True, # are the wheels controlled independantly or together
             # 'hip_kp': 0.0, # hip position control P gain
             'hip_kd': 0.0 # hip position control D gain
         }]
