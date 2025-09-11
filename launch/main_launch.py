832b002371ef22d755daa9c2962792d8e7d5d3b4
NaCl-Salt-12 <cla21052@byui.edu>
2025-09-11 17:16:54 -0400

    added wheel nodes

--- launch/main_launch.py
+++ launch/main_launch.py
@@ -28,6 +28,28 @@ def generate_launch_description():
             'joint_name': "hip",
         }]
     )
+    # wheel1 = Node(
+    #     package='cubemars_v2_ros',
+    #     executable='motor_node',
+    #     name='wheel1_motor_node',
+    #     namespace='wheel1',
+    #     parameters= [{
+    #         'can_id': 1,
+    #         'motor_type': "AK70-10",
+    #         'joint_name': "wheel1",
+    #     }]
+    # )
+    # wheel2 = Node(
+    #     package='cubemars_v2_ros',
+    #     executable='motor_node',
+    #     name='wheel2_motor_node',
+    #     namespace='wheel2',
+    #     parameters= [{
+    #         'can_id': 2,
+    #         'motor_type': "AK70-10",
+    #         'joint_name': "wheel2",
+    #     }]  
+    # )
 
     main_ctrl = Node(
         package='main_ctrl',
@@ -56,6 +78,8 @@ def generate_launch_description():
     return LaunchDescription([
         knee,
         hip,
+        # wheel1,
+        # wheel2,
         main_ctrl,
         joystick,
     ])
