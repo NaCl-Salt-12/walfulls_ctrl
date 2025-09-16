916ce321816f4ab7ccb4a280207a2e6ead5afa8a
NaCl-Salt-12 <cla21052@byui.edu>
2025-09-16 12:16:47 -0400

    testing wheels

--- launch/main_launch.py
+++ launch/main_launch.py
@@ -6,50 +6,50 @@ control_hz = 20
 
 
 def generate_launch_description():
-    knee = Node(
-        package='cubemars_v2_ros',
-        executable='motor_node',
-        name='knee_motor_node',
-        namespace='knee',
-        parameters= [{
-            'can_id': 4,
-            'motor_type': "AK80-64",
-            'joint_name': "knee",
-        }]
-    )
-    hip = Node(
-        package='cubemars_v2_ros',
-        executable='motor_node',
-        name='hip_motor_node',
-        namespace='hip',
-        parameters= [{
-            'can_id': 3,
-            'motor_type': "AK70-10",
-            'joint_name': "hip",
-        }]
-    )
-    # wheel1 = Node(
+    # knee = Node(
     #     package='cubemars_v2_ros',
     #     executable='motor_node',
-    #     name='wheel1_motor_node',
-    #     namespace='wheel1',
+    #     name='knee_motor_node',
+    #     namespace='knee',
     #     parameters= [{
-    #         'can_id': 1,
-    #         'motor_type': "AK70-10",
-    #         'joint_name': "wheel1",
+    #         'can_id': 4,
+    #         'motor_type': "AK80-64",
+    #         'joint_name': "knee",
     #     }]
     # )
-    # wheel2 = Node(
+    # hip = Node(
     #     package='cubemars_v2_ros',
     #     executable='motor_node',
-    #     name='wheel2_motor_node',
-    #     namespace='wheel2',
+    #     name='hip_motor_node',
+    #     namespace='hip',
     #     parameters= [{
-    #         'can_id': 2,
+    #         'can_id': 3,
     #         'motor_type': "AK70-10",
-    #         'joint_name': "wheel2",
-    #     }]  
+    #         'joint_name': "hip",
+    #     }]
     # )
+    wheel1 = Node(
+        package='cubemars_v2_ros',
+        executable='motor_node',
+        name='wheel1_motor_node',
+        namespace='wheel1',
+        parameters= [{
+            'can_id': 1,
+            'motor_type': "AK10-9",
+            'joint_name': "wheel1",
+        }]
+    )
+    wheel2 = Node(
+        package='cubemars_v2_ros',
+        executable='motor_node',
+        name='wheel2_motor_node',
+        namespace='wheel2',
+        parameters= [{
+            'can_id': 2,
+            'motor_type': "AK10-9",
+            'joint_name': "wheel2",
+        }]  
+    )
 
     main_ctrl = Node(
         package='main_ctrl',
@@ -76,10 +76,10 @@ def generate_launch_description():
         }]
     )
     return LaunchDescription([
-        knee,
-        hip,
-        # wheel1,
-        # wheel2,
+        # knee,
+        # hip,
+        wheel1,
+        wheel2,
         main_ctrl,
         joystick,
     ])
