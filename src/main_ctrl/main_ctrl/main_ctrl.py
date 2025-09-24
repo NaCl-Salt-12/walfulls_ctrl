e796d6476fcd0517d0603c48b90fdc0aa22f3c22
NaCl-Salt-12 <cla21052@byui.edu>
2025-09-24 15:11:10 -0400

    fix: adjust hip velocity calculation to remove time delta dependency

--- src/main_ctrl/main_ctrl/main_ctrl.py
+++ src/main_ctrl/main_ctrl/main_ctrl.py
@@ -212,7 +212,7 @@ class MainControlLoop(Node):
                 # Hip velocities
                 # self.des_hip_splay = self.des_hip_splay + dpad_ud * self.max_hip_vel * self.dt 
                 self.des_hip_splay = 0.0
-                self.des_hip_vel = dpad_ud * self.max_hip_vel * self.dt 
+                self.des_hip_vel = dpad_ud * self.max_hip_vel 
                 # self.des_hip_splay = max(min(self.des_hip_splay, self.max_hip_angle), self.min_hip_angle) # * 33.0 # convert to motor units due to gearing
 
                 # Create and publish knee command
